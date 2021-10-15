a1c7a949c47a704753cde0b2168a3fb40ae79e12
walshbr <bmw9t@virginia.edu>
2021-10-15 09:36:04 -0400

    update with code review on data collections

---
+++ data-review.py
@@ -0,0 +1,43 @@
+# data types
+
+# strings: sequence of characters
+my_string = 'a list is here'
+
+# integers: numbers
+my_int = 4
+
+# floats: numbers w/ decimals
+my_float = 4.0
+
+# data collections
+# list: property 1 - it has an order; property 2 - it has many things in it; 3 - you can do list-like things to it
+# my_list[2]
+
+my_list = ['a', 'list', 'is', 'here']
+my_list[2]
+
+# dictionary: property 1 - associative; property 2 - no order
+# my_dict = {key: value, key: value }
+my_dict = {'virginia': 'va', 'texas': 'tx'}
+my_dict['virginia']
+
+my_authors = {'dickens': 'hard times', 'woolf': 'orlando'}
+
+# ways of organizing data
+# let's organize the praxis cohort
+
+# a string - we could capitalize it or lowercase it
+the_string_cohort = "The Praxis cohort consists of Susan, Joseph, Jennifer, Jacqui, Tarushi, Cherrie"
+
+# a list
+the_list_cohort = ['Susan', 'Joseph', 'Jennifer', 'Jacqui', 'Tarushi', 'Cherrie']
+
+# a dictionary
+# first_name: last_name
+# initials: full name
+# cohort_num: list_of_students
+the_dict_cohort = {'Susan': 3, 'Joseph': 5}
+ 
+# book - author, death_date, title, list of words in the book
+book_one_list = ['Dickens', '1894', 'Hard Times', ['It', 'was', 'the', 'hardest', 'students']]
+book_one_dict = {'author': 'Dickens', 'title': 'Hard Times', 'the_words': ['It', 'was', 'the', 'hardest','students']}
