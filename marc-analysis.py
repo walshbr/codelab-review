3f21d9858971a39601d4dc5163236d5d08b6ff58
walshbr <bmw9t@virginia.edu>
2021-10-29 15:12:20 -0400

    Update

--- marc-analysis.py
+++ marc-analysis.py
@@ -1,10 +1,15 @@
-from os import read
-import bs4
+from bs4 import BeautifulSoup
 
 susan_file_to_work_with = "MANUSCRITOxml.xml"
 
 with open(susan_file_to_work_with, 'r') as file_in:
     read_file = file_in.read()
 
-soup = bs4.BeautifulSoup(read_file, 'html.parser')
-print(soup)
+soup = BeautifulSoup(read_file, 'html.parser')
+records = soup.find_all('marc:record')
+for record in records:
+    if record.find('marc:datafield', tag="300"):
+        print(record.find('marc:datafield', tag="300").text)
+    # for child in record.children:
+    #     print(child)
+    #     print(child.name)
